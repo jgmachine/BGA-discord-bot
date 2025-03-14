e2afa1218d849d1718d7b48784eb8ff6d0609525
Jesse <jgmachine@gmail.com>
2025-03-14 16:44:35 -0700

    make db persist

--- src/database.py
+++ src/database.py
@@ -1,56 +1,52 @@
 import sqlite3
 import logging
+import os
 from collections import namedtuple
+
+# ✅ Store SQLite in Railway's persistent directory
+DB_PATH = "/data/database.db"
 
 Game = namedtuple("Game", ["id", "url", "name", "activePlayerId"])
 
 
-class Game:
-    def __init__(self, id, url, name, activePlayerId):
-        self.id = id
-        self.url = url
-        self.name = name
-        self.activePlayerId = activePlayerId
-
-
 class Database:
-    def __init__(self, db_file):
-        self.db_file = db_file
+    def __init__(self):
+        os.makedirs("/data", exist_ok=True)  # Ensure the directory exists
+        self.db_file = DB_PATH
         self.conn = None
         self.cursor = None
 
     def connect(self):
+        """Open a connection to the database."""
         self.conn = sqlite3.connect(self.db_file)
         self.cursor = self.conn.cursor()
 
     def createTables(self):
+        """Create tables if they don't exist."""
         self.connect()
-
-        # Create the user_data table
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS user_data (
                 discord_id INTEGER PRIMARY KEY,
-                bga_id INTEGER
+                bga_id TEXT UNIQUE NOT NULL
             )
         """
         )
-
-        # Create the game table with a foreign key to user_data
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS game_data (
                 id INTEGER PRIMARY KEY,
-                url STRING,
-                game_name STRING,
+                url TEXT,
+                game_name TEXT,
                 active_player_id INTEGER
             )
         """
         )
-
         self.conn.commit()
+        self.close()
 
     def insertUserData(self, discordId, bgaId):
+        """Insert a user into the database."""
         self.connect()
         try:
             self.cursor.execute(
@@ -58,43 +54,30 @@ class Database:
                 (discordId, bgaId),
             )
             self.conn.commit()
-        except sqlite3.Error as e:
-            logging.error(f"SQLite error: {e}")
+        except sqlite3.IntegrityError:
+            logging.error(f"User with Discord ID {discordId} already exists.")
         finally:
             self.close()
 
     def deleteUserData(self, discord_id):
+        """Remove a user from the database."""
         self.connect()
-        try:
-            self.cursor.execute(
-                "DELETE FROM user_data WHERE discord_id = ?", (discord_id,)
-            )
-            self.conn.commit()
-
-        except sqlite3.Error as e:
-            logging.error(f"SQLite error: {e}")
-
-        finally:
-            self.close()
+        self.cursor.execute("DELETE FROM user_data WHERE discord_id = ?", (discord_id,))
+        self.conn.commit()
+        self.close()
 
     def getDiscordIdByBgaId(self, bga_id):
+        """Retrieve a Discord ID by BGA ID."""
         self.connect()
-        try:
-            self.cursor.execute(
-                "SELECT discord_id FROM user_data WHERE bga_id = ?", (bga_id,)
-            )
-            result = self.cursor.fetchone()
-            if result:
-                discord_id = result[0]
-                return discord_id
-
-        except sqlite3.Error as e:
-            print(f"SQLite error: {e}")
-
-        finally:
-            self.close()
+        self.cursor.execute(
+            "SELECT discord_id FROM user_data WHERE bga_id = ?", (bga_id,)
+        )
+        result = self.cursor.fetchone()
+        self.close()
+        return result[0] if result else None
 
     def insertGameData(self, id, url, gameName, activePlayerId):
+        """Insert a new game into the database."""
         self.connect()
         try:
             self.cursor.execute(
@@ -102,112 +85,64 @@ class Database:
                 (id, url, gameName, activePlayerId),
             )
             self.conn.commit()
-
         except sqlite3.Error as e:
             logging.error(f"SQLite error: {e}")
         finally:
             self.close()
 
     def deleteGameData(self, id):
+        """Remove a game from the database."""
         self.connect()
-        try:
-            self.cursor.execute("DELETE FROM game_data WHERE id = ?", (id,))
-            self.conn.commit()
-
-        except sqlite3.Error as e:
-            print(f"SQLite error: {e}")
-
-        finally:
-            self.close()
+        self.cursor.execute("DELETE FROM game_data WHERE id = ?", (id,))
+        self.conn.commit()
+        self.close()
 
     def updateActivePlayer(self, id, activePlayerId):
+        """Update the active player of a game."""
         self.connect()
-        try:
-            self.cursor.execute(
-                "UPDATE game_data SET active_player_id = ? WHERE id = ?",
-                (activePlayerId, id),
-            )
-            self.conn.commit()
-
-        except sqlite3.Error as e:
-            logging.error(f"SQLite error: {e}")
-
-        finally:
-            self.close()
+        self.cursor.execute(
+            "UPDATE game_data SET active_player_id = ? WHERE id = ?",
+            (activePlayerId, id),
+        )
+        self.conn.commit()
+        self.close()
 
     def getActivePlayer(self, id):
+        """Retrieve the active player of a game."""
         self.connect()
-        try:
-            self.cursor.execute(
-                "SELECT active_player_id FROM game_data WHERE id = ?", (id,)
-            )
-            result = self.cursor.fetchone()
-            if result:
-                active_player_id = result[0]
-                return active_player_id
-
-        except sqlite3.Error as e:
-            logging.error(f"SQLite error: {e}")
-
-        finally:
-            self.close()
+        self.cursor.execute("SELECT active_player_id FROM game_data WHERE id = ?", (id,))
+        result = self.cursor.fetchone()
+        self.close()
+        return result[0] if result else None
 
     def getGameById(self, game_id):
+        """Retrieve a game by its ID."""
         self.connect()
-        try:
-            self.cursor.execute(
-                "SELECT id, url, game_name, active_player_id FROM game_data WHERE id = ?",
-                (game_id,),
-            )
-            result = self.cursor.fetchone()
-
-            if result:
-                game = Game(
-                    id=result[0],
-                    url=result[1],
-                    name=result[2],
-                    activePlayerId=result[3],
-                )
-                return game
-            else:
-                logging.error(f"No data found for game ID {game_id}")
-                return None
-
-        except sqlite3.Error as e:
-            logging.error(f"SQLite error: {e}")
-            return None
-
-        finally:
-            self.close()
+        self.cursor.execute(
+            "SELECT id, url, game_name, active_player_id FROM game_data WHERE id = ?",
+            (game_id,),
+        )
+        result = self.cursor.fetchone()
+        self.close()
+        return Game(*result) if result else None
 
     def getAllGames(self):
+        """Retrieve all games."""
         self.connect()
-        try:
-            self.cursor.execute(
-                "SELECT id, url, game_name, active_player_id FROM game_data"
-            )
-            all_games = self.cursor.fetchall()
-
-            Game = namedtuple("Game", ["id", "url", "name", "activePlayerId"])
-
-            game_objects = [Game(*game_data) for game_data in all_games]
-
-            return game_objects
-        finally:
-            self.close()
+        self.cursor.execute("SELECT id, url, game_name, active_player_id FROM game_data")
+        games = self.cursor.fetchall()
+        self.close()
+        return [Game(*game) for game in games]
 
     def getAllBgaIds(self):
+        """Retrieve all BGA IDs."""
         self.connect()
-        try:
-            self.cursor.execute("SELECT bga_id FROM user_data")
-            rows = self.cursor.fetchall()
-            return [row[0] for row in rows]
-        except sqlite3.Error as e:
-            logging.error(f"SQLite error: {e}")
-
-        finally:
-            self.close()
+        self.cursor.execute("SELECT bga_id FROM user_data")
+        rows = self.cursor.fetchall()
+        self.close()
+        return [row[0] for row in rows]
 
     def close(self):
+        """Close the database connection."""
         if self.conn:
             self.conn.close()
